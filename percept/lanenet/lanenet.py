03912ed55d97a60b05db0d78bdb83a2477cad67f
肖尚华 <hallazie@gmail.com>
2020-04-17 00:58:55 +0800

    --amend

---
+++ percept/lanenet/lanenet.py
@@ -0,0 +1,10 @@
+# --*-- utf-8 --*--
+# @Author: Xiao Shanghua
+# @Date: 2020-04-17 00:34:15
+# @LastEditTime: 2020-04-17 00:34:16
+# @LastEditors: Xiao Shanghua
+# @Description: 
+# @FilePath: \machinelearning\vision\need-for-auto-speed\percept\lanenet\lanenet.py
+
+
+
